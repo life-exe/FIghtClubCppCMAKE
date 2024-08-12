496de8d46ecacb3c634f56b2b8c5a7993d87bd94
Life EXE <lifeexe.art@gmail.com>
2024-08-12 14:20:32 +0300

    Fix linux build

--- Source/Utils/Math.h
+++ Source/Utils/Math.h
@@ -1,12 +1,16 @@
 #pragma once
 #include <vector>
 
-#ifdef _EXPORTING
-#define MATH_API __declspec(dllexport)
-#elif _IMPORTING
-#define MATH_API __declspec(dllimport)
+#if defined(WIN32) || defined(_WINDOWS)
+    #ifdef _EXPORTING
+        #define MATH_API __declspec(dllexport)
+    #elif _IMPORTING
+        #define MATH_API __declspec(dllimport)
+    #else
+        #define MATH_API
+    #endif
 #else
-#define MATH_API
+    #define MATH_API __attribute__ ((visibility ("default")))
 #endif
 
 namespace LifeExe
