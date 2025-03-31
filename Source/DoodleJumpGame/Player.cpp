2f04e735213acb02a8b223f6555746635117372f
life-exe <lifeexe.art@gmail.com>
2025-03-31 21:06:49 +0300

    Add extra Player ctor

--- Source/DoodleJumpGame/Player.cpp
+++ Source/DoodleJumpGame/Player.cpp
@@ -1,6 +1,8 @@
 #include "Player.h"
 #include <algorithm>
 
+Player::Player(const ScreenSize& screenSize) : Player(screenSize, Config()) {}
+
 Player::Player(const ScreenSize& screenSize, const Config& config)  //
     : c_screenSize(screenSize), c_config(config), m_velocityY(0.0f)
 {
